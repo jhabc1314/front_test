f8fb18985dddf0bd51e61a32e4de407baa51a94c
jh <jiangheng@chelun.com>
2021-02-03 15:17:56 +0800

    init

--- chelun/js/normal.js
+++ chelun/js/normal.js
@@ -4,7 +4,7 @@
     document.addEventListener("DOMContentLoaded", () => {
         const html = document.querySelector("html");
         let fontSize = window.innerWidth * MAX_FONT_SIZE / MAX_WIDTH;
-        console.log(fontSize);
+        console.log(fontSize, window.innerWidth);
         fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize;
         html.style.fontSize = fontSize + 'px';
     })
